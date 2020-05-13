4862ea4a8b1589b66e5d6b5b8c4469f0f4545080
Paa J A K E <paajake@gmail.com>
2020-05-13 15:50:44 +0000

    Adds uncommitted files

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class UserResource
+ * @mixin  \App\User
+ */
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => strval($this->id),
+            'type' => 'users',
+            'attributes' => [
+                'name' => $this->name,
+                'email' => $this->email,
+                'type' => $this->type,
+                'image' => $this->image,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ]
+        ];
+    }
+}
