c68097b7c5532fd7483d3d727235801c2f82dc4e
Paa J A K E <paajake@gmail.com>
2020-05-18 11:44:05 +0000

    Adds codecov integration to ci pipeline

---
+++ app/Http/Resources/TokenResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Laravel\Sanctum\PersonalAccessToken;
+
+/**
+ * Class TokenResource
+ * @mixin  PersonalAccessToken
+ */
+class TokenResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $user = User::find($this->accessToken->id);
+        return [
+            'id' => strval($this->accessToken->id),
+            'type' => 'users',
+            'attributes' => [
+                'device_name' => $this->accessToken->name,
+                'token' => $this->plainTextToken,
+                'abilities' => $this->accessToken->abilities,
+                'last_used_at' => $this->accessToken->last_used_at,
+                'created_at' => $this->accessToken->created_at,
+                'updated_at' => $this->accessToken->updated_at,
+            ],
+            "relationships" => [
+                "users" => [
+                    "links" => [
+                        "self" => "",
+                        "related" => ""
+                    ],
+                    "data" => [
+                        [
+                            "id" => $this->accessToken->tokenable_id,
+                            "type" => "users"
+                        ]
+                    ]
+                ]
+            ],
+            "included" => [
+                [
+                    "id" => strval($this->accessToken->tokenable_id),
+                    "type"=> "users",
+                    "attributes"=> [
+                        "name"=> $user->name,
+                        "email"=> $user->email,
+                        "image"=> $user->image
+                    ]
+                ]
+            ]
+        ];
+    }
+}
